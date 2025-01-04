6f687d16742bd51c8c5d3632f64ffc2a08c6936d
carinalanchine <carinalanchine@gmail.com>
2025-01-04 01:25:46 -0300

    feat: refactor Equipamentos and Inventario + update Jogador

--- src/game/classes/EquipamentoMaos.class.ts
+++ src/game/classes/EquipamentoMaos.class.ts
@@ -1,17 +1,19 @@
 import { Classe } from "./Classe.class";
 import { Equipamento } from "./Equipamento.class";
+import { Jogador } from "./Jogador.class";
+import { Jogo } from "./Jogo.class";
 import { Raca } from "./Raca.class";
 
-export class EquipamentoMaos extends Equipamento{
-    constructor(
-        nome: string,
-        descricao: string,
-        grande: boolean,
-        valor: number,
-        bonus: number,
-        restricoesRaca: Raca[],
-        restricoesClasse: Classe[],
-    ) {
-        super(nome, descricao, grande, valor, bonus, restricoesRaca, restricoesClasse);
-    }
+export class EquipamentoMaos extends Equipamento {
+  constructor(
+    nome: string,
+    descricao: string,
+    grande: boolean,
+    valor: number,
+    bonus: number,
+    restricoesRaca: Raca[],
+    restricoesClasse: Classe[],
+  ) {
+    super(nome, descricao, grande, valor, bonus, restricoesRaca, restricoesClasse);
+  }
 }
